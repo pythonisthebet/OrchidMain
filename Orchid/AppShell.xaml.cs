0239fe57a2441052d1629eb1466f82ccf4fed972
User <User@LAB3-3>
2024-10-07 13:01:43 +0300

    Add project files.

---
+++ Orchid/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Orchid
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Orchid/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Orchid
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
