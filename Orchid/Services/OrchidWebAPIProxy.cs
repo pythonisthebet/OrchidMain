5e4d1b6af039c26515bda4c357bc2b391d38cbbf
User <User@LAB3-3>
2024-10-07 14:59:56 +0300

    added proxy and started working on it plus annabeled deve tunneling

---
+++ Orchid/Services/OrchidWebAPIProxy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+//using Orchid.Models;
+
+namespace Orchid.Services
+{
+    public class OrchidWebAPIProxy
+    {
+        #region without tunnel
+        /*
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "localhost";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5110/api/" : $"http://{serverIP}:5110/api/";
+        private static string ImageBaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5110" : $"http://{serverIP}:5110";
+        */
+        #endregion
+
+        #region with tunnel
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "th3r7mpp-7021.euw.devtunnels.ms";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = "https://th3r7mpp-7021.euw.devtunnels.ms/api/";
+        private static string ImageBaseAddress = "https://th3r7mpp-7021.euw.devtunnels.ms/";
+        #endregion
+
+        public OrchidWebAPIProxy()
+        {
+            //Set client handler to support cookies!!
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = new System.Net.CookieContainer();
+
+            this.client = new HttpClient(handler);
+            this.baseUrl = BaseAddress;
+        }
+
+        public string GetImagesBaseAddress()
+        {
+            return OrchidWebAPIProxy.ImageBaseAddress;
+        }
+
+        public string GetDefaultProfilePhotoUrl()
+        {
+            return $"{OrchidWebAPIProxy.ImageBaseAddress}/profileImages/default.png";
+        }
+    }
+}
